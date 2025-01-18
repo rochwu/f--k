7a8f3a10c331b8708bfb7f40237881c6404d6b29
Rolando Wu <rolando.che.wu@gmail.com>
2025-01-18 03:54:00 -0800

    Removed uneeded preventDefault

--- src/index.tsx
+++ src/index.tsx
@@ -11,16 +11,6 @@ document.addEventListener(
   {passive: false},
 );
 
-// Stops magnifying glass on double tap + move
-// Stops "Share..." prompt from showing up
-document.addEventListener(
-  'touchend',
-  (e) => {
-    // e.preventDefault();
-  },
-  {passive: false},
-);
-
 const root = document.getElementById('root');
 
 render(() => <App />, root!);
